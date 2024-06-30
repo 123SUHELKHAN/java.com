d8f025f43f8106f0f5dd5d9a0aefd2d71f57e1f9
123SUHELKHAN <156664896+123SUHELKHAN@users.noreply.github.com>
2024-06-30 13:05:08 +0530

    Add files via upload
    welcome

---
+++ Problem1.js
@@ -0,0 +1,6 @@
+/*var today = new Date(),hours = today.getHours(),minutes = today.getMinutes(),seconds = today.getSeconds();
+var formate2 = hours + ":" + minutes + ":" + seconds;
+//Date formate
+document.write(formate2);*/
+
+
